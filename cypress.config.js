4f52b70a20b0a304a431508dbec97db1731aad61
Ana Carneiro <ana.carneiro@bark.com>
2023-05-01 23:05:49 +0100

    update to typescript

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  baseUrl: "https://qa-7.d.bark.com",
+  baseUrl: "https://commitquality.com/",
   resolution: "1920x1080",
   viewportWidth: 1920,
   viewportHeight: 1080,
-  defaultCommandTimeout: 30000,
-  responseTimeout: 30000,
-  taskTimeout: 30000,
+  defaultCommandTimeout: 1000,
+  responseTimeout: 1000,
+  taskTimeout: 1000,
   scrollBehavior: "center",
   chromeWebSecurity: false,
   fixtures: true,
-  videoUploadOnPasses: true,
+  videoUploadOnPasses: false,
   experimentalWebKitSupport: true,
   experimentalInteractiveRunEvents: true,
+  numTestsKeptInMemory: 3,
   retries: {
     runMode: 3,
     openMode: 0,
   },
+  specPattern: "**/*.cy.ts",
   e2e: {
     setupNodeEvents(on, config) {},
   },
